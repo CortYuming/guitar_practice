3005859777e2267aa83cefcf8ee13debe88b220d
Takashi ITO <cortyuming@gmail.com>
2017-10-29 15:50:15 +0900

    Modify Display randome key

--- js/random_key.js
+++ js/random_key.js
@@ -2,6 +2,13 @@ $(function(){
   var tempo = 50;
   var delay = 60 / tempo * 1000;
 
+  function set_display_key_width() {
+    var screen_width = window.parent.screen.width;
+    var $key = $('.js-key');
+    $key.css('width', screen_width);
+
+    console.info(screen_width);
+  }
   function random(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
   }
@@ -10,7 +17,8 @@ $(function(){
     var key = random(keys).replace(/(.+?\/)/g, '<span style="color:#eee;">$1</span>');
     $('.js-key').html(key);
   }
-  
+
+  set_display_key_width();
   display_key();
   setInterval(function(){
     display_key();
